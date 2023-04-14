1b630138670e378063673b370b7eefd8f7d85160
dale <hessdale96@gmail.com>
2023-04-14 12:22:11 -0600

    added in page components

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+<img width="150px" src="../assets/logo.png" alt="logo">
+<nav>
+    <p>link</p>
+</nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+div{
+    display: grid;
+    justify-items: center;
+    grid-auto-columns: 1fr 1fr;
+}
+</style>
